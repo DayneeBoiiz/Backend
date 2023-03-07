cc9c677c54da522087a20db6c9120428a119a305
DayneeBoiiz <saad.ayar@hotmail.com>
2023-03-07 18:35:07 +0100

    Backend

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
-import { ServiceModule } from './controller/service/service.module';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, ServiceModule, UserModule, PrismaModule],
+  imports: [AuthModule, UserModule, PrismaModule],
 })
 export class AppModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,59 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+// import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signin() {
-    return 'Im signing in';
+  async signin(dto: AuthDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    if (!user) {
+      throw new ForbiddenException('User not found');
+    }
+
+    const pwMatches = await argon.verify(user.hash, dto.password);
+    if (!pwMatches) {
+      throw new ForbiddenException('Incorrect password');
+    }
+
+    delete user.hash;
+    return user;
   }
 
-  signup() {
-    return 'Im signing up';
+  async signup(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash: hash,
+          firstName: dto.FirstName,
+          NickName: dto.NickName,
+          LastName: dto.LastName,
+        },
+      });
+
+      delete user.hash;
+      return user;
+    } catch (error) {
+      // if (error instanceof PrismaClientKnownRequestError) {
+      if (error.code == 'P2002') {
+        // console.log(error);
+        throw new ConflictException('Credentials taken');
+      }
+      // }
+      // throw error;
+    }
   }
 }
